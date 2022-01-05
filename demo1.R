f0517e5ec4821539837481d7e9eb16667aa65be4
petcheme <54142875+petcheme@users.noreply.github.com>
2022-01-05 17:00:50 -0300

    Update demo1.R

--- demo1.R
+++ demo1.R
@@ -5,9 +5,15 @@ library(ggplot2)
 
 # demo code for the initial upload
 
+# responses
 my_file <- file.path("data", "data_Psycurve_R_trial.csv")
-
 data <- read_csv(my_file)
 
 ggplot(data %>% group_by(Distance_lin, Distance_log) %>% summarise(Response = mean(Response)),
        aes(x=Distance_log, y=Response)) + geom_line()
+
+# subjects
+my_file <- file.path("data", "data_Reach_R.csv")
+subjects <- read_csv(my_file) 
+
+subjects %>% summarise(Reach = mean(Reach))
