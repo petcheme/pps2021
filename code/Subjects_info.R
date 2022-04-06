af2672431f06e7c987728ae1b22ee4c89453de6b
petcheme <54142875+petcheme@users.noreply.github.com>
2022-04-06 17:33:26 -0300

    Update Subjects_info.R
    Read excluded subjects from table. Report excluded subjects. Also report hearing and vision problems.

--- code/Subjects_info.R
+++ code/Subjects_info.R
@@ -1,29 +1,57 @@
 # Load libraries
-pacman::p_load(here, 
-               ggplot2, 
-               magrittr, 
+pacman::p_load(default,
+               here,
+               ggplot2,
+               magrittr,
                tidyverse)
 
 # Clear workspace
 rm(list = ls())
 
+# Some default values
+default(read_csv) <- list(lazy = FALSE,
+                      progress = FALSE,
+                show_col_types = FALSE,
+                       comment = "#")
 
 # Load data
-data_subjects <- read_csv(here("data", "data_demographics.csv"),
-                 comment = "#",
-                 lazy = FALSE) %>%
-  # This subject could not complete the experiment
-  filter(Subject != "S64")
+data_demographics <- read_csv(here("data", "data_demographics.csv"))
+
+# Report excluded subjects, then exclude them
+data_demographics %>%
+  filter(Excluded == TRUE) %>%
+  select(Experiment, Subject, Gender, Age, Excluded, `Exclusion Motive`)
+
+data_demographics_excluded <- data_demographics %>%
+  filter(Excluded == TRUE)
+
+data_demographics %<>% filter(Excluded == FALSE) 
 
 
 # Obtain summary statistics
-stats_subjects <- data_subjects %>%
+stats_demographics <- data_demographics %>%
+
   group_by(Experiment) %>%
   summarise(n = n(),
-            nFemale   = sum(Gender == "F"),
-            nMusician = sum(`Years of Musical Education` > 0),
-            nHearProb = sum(str_to_upper(`Hearing Problems`) != "NO"),
+            n_Fem     = sum(Gender == "F"),
+            n_Mus     = sum(`Years of Musical Education` > 0),
+            n_HearProb = sum(str_to_upper(`Hearing Problems`) != "NO"),
+            n_VisiProb = sum(str_to_upper(`Vision Problems`) != "NO"),
             Age_mean  = mean(Age),
             Age_sd    = sd(Age),
             Age_min   = min(Age),
             Age_max   = max(Age))
+
+stats_demographics
+
+# Hearing problems
+data_demographics %>%
+  filter(str_to_upper(`Hearing Problems`) != "NO") %>%
+  select(Experiment, Subject, Gender, Age, `Hearing Problems`)
+
+# Vision problems
+data_demographics %>%
+  filter(str_to_upper(`Vision Problems`) != "NO") %>%
+  select(Experiment, Subject, Gender, Age, `Vision Problems`)
+
+
