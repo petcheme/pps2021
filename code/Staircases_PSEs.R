e23836828d2d8b15abca7ca5bd0f4263fb306b66
petcheme <54142875+petcheme@users.noreply.github.com>
2022-05-12 15:41:24 -0300

    Update Staircases_PSEs.R
    Minor updates, also updated comments after exclusion of reversals out of range

--- code/Staircases_PSEs.R
+++ code/Staircases_PSEs.R
@@ -7,16 +7,22 @@ pacman::p_load(here,
 # Clear workspace
 rm(list = ls())
 
+# Some default values
+default(read_csv) <- list(lazy = FALSE,
+                      progress = FALSE,
+                show_col_types = FALSE,
+                       comment = "#")
+
 # Analysis parameters
 par.first_n_reversals_discarded <- 2
 par.alpha <- 0.05
 
 # Load data
 data_stair <- here("data", "data_exp1_staircases.csv") %>%
-  read_csv(lazy = FALSE) %>%
+  read_csv() %>%
   mutate(Exp = "1") %>%
   rbind(here("data", "data_exp2_staircases.csv") %>%
-          read_csv(lazy = FALSE) %>%
+          read_csv() %>%
           mutate(Exp = "2")) %>%
   mutate(Exp     = factor(Exp),
          Subject = factor(Subject)) %>%
@@ -31,9 +37,11 @@ info_subjects <- data_stair %>% ungroup() %>% distinct(Exp, Subject)
 
 # Each subject provided six blocks (dual blocks had two staircases each,
 # hence each subject performed eight staircases)
-info_blocks <- data_stair %>% ungroup() %>% distinct(Condition, Block)
+info_blocks <- data_stair %>% ungroup() %>% distinct(Condition, Block) %>%
+  arrange(Condition, Block)
 
-# Conditions performed by each subject (48 subjects x 4 combinations of conditions = 192 rows)
+# Conditions performed by each subject (48 subjects x 4 combinations of
+# conditions, minus 5 rows excluded due to reversals out of range = 187 rows)
 info_conds_subjects <- data_stair %>%
   ungroup() %>%
   group_by(Subject, Condition, Block, Start) %>% 
@@ -41,9 +49,15 @@ info_conds_subjects <- data_stair %>%
   group_by(Subject, Condition, Start) %>%
   summarise(n= n())
 
-info_conds_subjects2 <- info_conds_subjects %>%
+# Summary of blocks performed by each subject
+info_conds_subjects_summary <- info_conds_subjects %>%
   group_by(Subject) %>%
   summarise(n = sum(n))
+
+# 278 blocks completed (48 subjects x 6 blocks, minus 10 excluded blocks due to
+# reversals out of range)
+info_conds_subjects_summary %$% sum(n)
+
 
 # There were four conditions (in the table, Condition means Staircase Type)
 # Simple staircases had 4 repetitions, dual had 2
@@ -51,16 +65,23 @@ info_conds <- info_conds_subjects %>% ungroup() %>%
   group_by(Condition, Start, n) %>% 
   distinct(Condition, Start, n)
 
+# Some conditions had some data excluded, hence some cases appear duplicated 
+# with a lower n...
+info_conds %>% arrange(Condition, Start, n)
+
+# ...the experimental design was this:
+info_conds %>% group_by(Condition, Start) %>% summarize(n = max(n))
+
 
 # --- Here starts data extraction ---
 
 # Grouping at the lowest level -includes StairId for dual staircases
 data_stair %<>% group_by(Exp, Subject, Condition, Block, Start, StairId)
 
-# How many groups: 376 (48 subjects x 8 staircases)
+# How many groups: 370 (48 subjects x 8 staircases each - 14 staircases excluded)
 data_stair %>% summarise(n=n())
 
-# Filter reversals. There are 4509 reversals: 376 blocks x 12 reversals each,
+# Filter reversals. There are 4437 reversals: 370 staircases x 12 reversals each,
 # minus three reversals lost in the way)
 data_reversals <- data_stair %>% filter(Reversal > 0) 
 
@@ -73,7 +94,7 @@ stat_reversals <- data_reversals %>%
                n = n(), .groups = "keep")
 
 # Lost reversals: S01 and S66
-stat_reversals %>% filter (n != 11)
+stat_reversals %>% filter (n != 10)
 
 # Collapse dual-near and dual-far into a single condition: dual
 # (Should check that there are not significant differences)
@@ -115,7 +136,3 @@ ggplot(data = stat_pse) +
                 aes(x = Condition, y = m, ymin = low, ymax=upp),width=0.2) +
   geom_point(data = mean_pse,
              aes(x = Condition, y = m), color = "red", size=4, alpha=0.5)
-
-
-
-
