2c7cf3c3914aa36abe09c322c6b09b5f4952b215
petcheme <54142875+petcheme@users.noreply.github.com>
2022-06-21 23:33:25 -0300

    Update Staircases_example.R
    Added example for Exp. 2, also minor changes

--- code/Staircases_example.R
+++ code/Staircases_example.R
@@ -23,6 +23,10 @@ par <- list("exp" = 1,
         "subject" = "S06",
       "max_trial" = 30)
 
+# par <- list("exp" = 2,
+#         "subject" = "S56",
+#       "max_trial" = 26)
+
 # Load data
 data_stair <- 
   rbind(here("data", "data_exp1_staircases.csv") %>% 
@@ -99,7 +103,7 @@ data_exp_dual <- data_stair %>%
   
   # this mutate should not be necessary
   mutate(Branch = if_else((Start == "near" & StairId == 1) |
-                            (Start == "far"  & StairId == 2),
+                          (Start == "far"  & StairId == 2),
                           "near", "far")) %>%
   
   group_by(Branch, Trial) %>%
@@ -114,12 +118,11 @@ data_exp_dual <- data_stair %>%
 
 # Filter subject data
 data_subject <- data_stair %>%
-  filter(Subject == par$subject) %>%
-  filter(Trial <= par$max_trial)
+  filter(Subject == par$subject)
 
 # Simple staircases
 data_panel1.1 <- data_subject %>%
-  filter(Condition == "simple", Block < 3) 
+  filter(Condition == "simple", Block == 1 | Block == 2)
 
 # some testing code
 # data_panel1.1 %>%
@@ -128,8 +131,8 @@ data_panel1.1 <- data_subject %>%
 #   summarise(Dist_mean = mean(Distance))
 
 panel1.1 <- data_panel1.1 %>%
-  ggplot(aes(x=Trial, y=Distance, group=Start)) +
-    geom_point(aes(color = Start)) + 
+  ggplot(aes(x=Trial, y=Distance, group=Block)) +
+    geom_point(aes(color = Start, shape = (Reversal > 2))) +
     geom_line(aes(color = Start)) +
     # scale_color_manual(values = c("#F8766D", "#00BFC4")) +
     scale_y_continuous(trans='log10') +
@@ -142,7 +145,14 @@ panel1.1 <- data_panel1.1 %>%
                  summarise(Dist_mean = mean(Distance)),
              aes(yintercept = Dist_mean,
                  color = BranchStart),
-             linetype = "dashed")
+             linetype = "dashed") #+
+  #geom_hline(yintercept = 88.97) + geom_hline(yintercept = 139.27)
+
+data_panel1.1 %>%
+  filter(Reversal > 2) %>%
+  filter(!(OutlierDist | OutlierSubj)) %>%
+  group_by(Subject, BranchStart) %>%
+  summarise(Dist_mean = mean(Distance)) %$% Dist_mean
 
 panel1.1
 
@@ -152,7 +162,7 @@ data_panel1.2 <- data_subject %>%
 
 panel1.2 <- data_panel1.2 %>%
   ggplot(aes(x=Trial, y=Distance, group = BranchStart)) +
-    geom_point(aes(color = BranchStart)) + 
+    geom_point(aes(color = BranchStart)) +
     geom_line(aes(color = BranchStart)) +
     scale_y_continuous(trans='log10') +
     #scale_color_manual(values = c("#00BFC4", "#F8766D")) +
@@ -165,8 +175,15 @@ panel1.2 <- data_panel1.2 %>%
                  summarise(Dist_mean = mean(Distance)),
                aes(yintercept = Dist_mean,
                    color = BranchStart),
-               linetype = "dashed")
-  
+               linetype = "dashed") #+
+ #geom_hline(yintercept = 131.12) + geom_hline(yintercept = 140.28)
+
+data_panel1.2 %>%
+  filter(Reversal > 2) %>%
+  filter(!(OutlierDist | OutlierSubj)) %>%
+  group_by(Subject, BranchStart) %>%
+  summarise(Dist_mean = mean(Distance)) %$% Dist_mean
+
 panel1.2
 
 #### 2ND ROW: Average target position ####
@@ -179,12 +196,12 @@ panel2.1 <- data_exp_simple %>%
   ylab("Target distance [cm]") +
   # average pse
   geom_hline(data = mean_pse %>%
-               filter(Condition == "simple"), 
+               filter(Condition == "simple"),
              aes(yintercept = Dist_mean, color = BranchStart),
              linetype = "dashed") +
   # average first reversal
   geom_vline(data = mean_fr %>%
-               filter(Condition == "simple"), 
+               filter(Condition == "simple"),
              aes(xintercept = Trial_mean, color = BranchStart),
              linetype = "dashed")
   
@@ -195,12 +212,12 @@ panel2.2 <- data_exp_dual %>%
     scale_y_continuous(trans='log10') +
     # average pse
     geom_hline(data = mean_pse %>%
-               filter(Condition == "dual"), 
+               filter(Condition == "dual"),
              aes(yintercept = Dist_mean, color = BranchStart),
              linetype = "dashed") +
     # average first reversal
     geom_vline(data = mean_fr %>%
-               filter(Condition == "dual"), 
+               filter(Condition == "dual"),
                aes(xintercept = Trial_mean, color = BranchStart),
                linetype = "dashed") +
     ylab(NULL)
@@ -211,28 +228,54 @@ panel2.2 <- data_exp_dual %>%
 panel3.1 <- data_exp_simple %>%
   ggplot(aes(x=Trial, y=RTlog_mean, group=Start)) +
   geom_point(aes(color = Start)) + geom_line(aes(color = Start)) +
-  ylab("Response time [log10(s)]") +
+  ylab("Response time [s]") +
   geom_vline(data = mean_fr %>%
              filter(Condition == "simple"), 
              aes(xintercept = Trial_mean, color = BranchStart),
-             linetype = "dashed")
+             linetype = "dashed") +
+  scale_y_continuous(breaks = log10(seq(.7, 1.6, by = 0.1)),
+                     labels = seq(.7, 1.6, by = 0.1),
+                     limits = log10(c(0.7, 1.6)))
 
 # Dual staircases
 panel3.2 <- data_exp_dual %>%
   ggplot(aes(x=Trial, y=RTlog_mean, group=Branch)) +
   geom_point(aes(color = Branch)) + geom_line(aes(color = Branch)) +
-  ylab("Response time [log10(s)]") +
   geom_vline(data = mean_fr %>%
              filter(Condition == "dual"), 
              aes(xintercept = Trial_mean, color = BranchStart),
-             linetype = "dashed")
+             linetype = "dashed") +
+  scale_y_continuous(breaks = log10(seq(.7, 1.6, by = 0.1)),
+                     labels = seq(.7, 1.6, by = 0.1),
+                     limits = log10(c(0.7, 1.6))) +
+  ylab(NULL)
 
 #### PATCHWORK PLOTS ####
 
-(panel1.1 + labs(subtitle="Simple") + theme(plot.subtitle = element_text(hjust = 0.5))  |
- panel1.2 + labs(subtitle="Dual")   + theme(plot.subtitle = element_text(hjust = 0.5))) /
-(panel2.1 + labs(subtitle="Simple") + theme(plot.subtitle = element_text(hjust = 0.5))  | 
- panel2.2 + labs(subtitle="Dual")   + theme(plot.subtitle = element_text(hjust = 0.5))) /
-(panel3.1 | 
- panel3.2)
+(panel1.1 + labs(subtitle="Simple") +
+   theme_classic() +
+   theme(plot.subtitle = element_text(hjust = 0.5),
+       legend.position = "none") |
+ panel1.2 + labs(subtitle="Dual")   +
+   theme_classic() +
+   theme(plot.subtitle = element_text(hjust = 0.5),
+       legend.position = "none")) /
+
+(panel2.1 + labs(subtitle="Simple") +
+   theme_classic() +
+   theme(plot.subtitle = element_text(hjust = 0.5),
+       legend.position = "none")  |
+ panel2.2 + labs(subtitle="Dual")   +
+   theme_classic() +
+   theme(plot.subtitle = element_text(hjust = 0.5),
+       legend.position = "none")) /
+
+(panel3.1 + labs(subtitle="Simple") +
+   theme_classic() +
+   theme(plot.subtitle = element_text(hjust = 0.5),
+       legend.position = "none") |
+ panel3.2 + labs(subtitle="Dual")   +
+  theme_classic() +
+  theme(plot.subtitle = element_text(hjust = 0.5),
+      legend.position = "none"))
 
